b240b931363f825d87b8bcf30005c67d463e9824
theriley106 <christopherlambert106@gmail.com>
2020-01-19 20:06:01 -0800

    Added working redirect

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+from flask import Flask, redirect, request
+from datetime import date
+import requests
+import json
+
+app = Flask(__name__)
+
+RANDOM_QUESTION_URL = "https://leetcode.com/problems/random-one-question/all"
+
+MAX_RETRY = 3
+
+def get_today_date():
+	return date.today().strftime("%B %d, %Y")
+
+def generate_new_question(history):
+	previous_urls = set(history.values())
+	for i in range(MAX_RETRY):
+		res = requests.get(RANDOM_QUESTION_URL, timeout=5)
+		if res.url not in previous_urls:
+			return res.url
+	return res.url
+
+def get_question(force_new=False):
+	dateVal = get_today_date()
+	if os.path.exists("archive.json") == False:
+		history = {}
+	else:
+		history = json.load(open("archive.json"))
+
+	if dateVal not in history or force_new:
+		question = generate_new_question(history)
+		history[dateVal] = question
+		write_to_json_file(history)
+	else:
+		question = history[dateVal]
+	return question
+
+def write_to_json_file(newData):
+	with open("archive.json", 'w') as f:
+		json.dump(newData, f)
+
+@app.route('/')
+def hello():
+	force_reload = request.args.get('force_reload') != None
+	print force_reload
+	question = get_question(force_reload)
+	return redirect(question, code=302)
+
+if __name__ == '__main__':
+	print get_question()
+	# Bind to PORT if defined, otherwise default to 5000.
+	app.run(host='0.0.0.0', port=5000)
+
+# import json
+
+
+
