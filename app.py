acd4a1ab45237b26d00dc3ffcdb03c8832c31017
theriley106 <christopherlambert106@gmail.com>
2020-02-11 20:59:29 -0800

    Removed default json response

--- app.py
+++ app.py
@@ -60,7 +60,6 @@ def write_to_json_file(newData):
 
 @app.route('/archive')
 def archive():
-	return jsonify({"ayy": "ayy"})
 	if os.path.exists("archive.json") == False:
 		write_to_json_file({})
 	return jsonify(json.load(open("archive.json")))
